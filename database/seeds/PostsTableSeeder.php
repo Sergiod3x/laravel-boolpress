10a93c636e7ef88eef790e446cb84d15889415a6
Sergiod3x <sergiod3x.@gmail.com>
2021-09-13 19:42:06 +0200

    start

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $posts=[
+            [
+                'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
+            ],
+            [
+                'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
+            ],
+            [
+                'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
+            ],
+        ];
+        foreach($posts as $post) {
+            $postObject=new Post();
+            $postObject->ap_partenza=$post['text'];
+            $postObject->save();
+        }
+    }
+}
+
