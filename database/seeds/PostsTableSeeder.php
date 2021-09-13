e00ca3c830930d8eb972ef542dff018dc40f867a
Sergiod3x <sergiod3x.@gmail.com>
2021-09-14 00:11:11 +0200

    faker ok

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,25 +10,37 @@ class PostsTableSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+     * 
      */
-    public function run()
+
+    public function run(Faker $faker)  // in maniera del tutto automatica, Laravel andrà a soddisfare questa dipendenza.
     {
-        $posts=[
-            [
-                'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
-            ],
-            [
-                'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
-            ],
-            [
-                'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
-            ],
-        ];
-        foreach($posts as $post) {
-            $postObject=new Post();
-            $postObject->text=$post['text'];
-            $postObject->save();
+
+        for ($i = 0; $i < 50; $i++){
+
+            $bookObject = new Post();
+            $bookObject->text = $faker->paragraph(2);
+            $bookObject->save();
         }
+
+    // public function run()
+    // {
+    //     $posts=[
+    //         [
+    //             'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
+    //         ],
+    //         [
+    //             'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
+    //         ],
+    //         [
+    //             'text'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem omnis repellat sed culpa velit reiciendis non quae beatae recusandae saepe fugit, officia vel similique. Sit quisquam deleniti earum magni.',
+    //         ],
+    //     ];
+    //     foreach($posts as $post) {
+    //         $postObject=new Post();
+    //         $postObject->text=$post['text'];
+    //         $postObject->save();
+    //     }
     }
 }
 
