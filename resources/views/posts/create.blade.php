744acb5ced095c475de9a63f785c44334e872c08
Sergiod3x <sergiod3x.@gmail.com>
2021-09-17 09:02:06 +0200

    fix

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,17 +2,24 @@
 
 @section("content")
 
-<form action='{{route("post.store")}}' method='POST'>
-    @csrf
-    <div class="form-group">
-        <label for="text">Post</label>
-        <textarea class="form-control" id ="text" rows="3"></textarea>
-        <!-- <input type="text" name="text" id ="text"> -->
+<div class='container'>
+    <div class='row'>
+        <div class='col-12'>
+            <form action='{{route("post.store")}}' method='POST'>
+                @csrf
+                <div class="form-group">
+                    <label for="text">Post</label>
+                    <textarea class="form-control" name="text" id ="text" rows="3"></textarea>
+                    <!-- <input type="text" name="text" id ="text"> -->
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Invia">
+                    <a href="/post"><i class="bi bi-arrow-90deg-left"></i></a>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <input type="submit" value="Invia">
-        <a href="/post"><i class="bi bi-arrow-90deg-left"></i></a>
-    </div>
-</form>
+</div>
+
 
 @endsection
