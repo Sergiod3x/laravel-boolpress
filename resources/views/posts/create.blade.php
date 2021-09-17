195a3a590ee0f4756e5c1afa2df07c04ef8224cc
Sergiod3x <sergiod3x.@gmail.com>
2021-09-17 09:43:54 +0200

    validation ok

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,17 @@
 
 @section("content")
 
+
 <div class='container'>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error) 
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <div class='row'>
         <div class='col-12'>
             <form action='{{route("post.store")}}' method='POST'>
